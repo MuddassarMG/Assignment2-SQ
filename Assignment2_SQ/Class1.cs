2bc0d3ad964bc682bfe647b21ed7199eba55f74b
Muddassar <shaikhmuddsar786@gmail.com>
2024-03-06 19:06:07 -0500

    Isosceles Triangle

--- Assignment2_SQ/Class1.cs
+++ Assignment2_SQ/Class1.cs
@@ -24,5 +24,45 @@ namespace Assignment2_SQ
             // Assert
             Assert.AreEqual("Equilateral triangle", result);
         }
+
+        // Valid Isosceles Triangles
+        [Test]
+        public void Triangle_TwoEqualSides_OutputIsosceles()
+        {
+            // Arrange
+            int sideA = 5, sideB = 5, sideC = 3;
+
+            // Act
+            var result = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void Triangle_TwoOtherEqualSides_OutputIsosceles()
+        {
+            // Arrange
+            int sideA = 5, sideB = 3, sideC = 5;
+
+            // Act
+            var result = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void Triangle_BaseAndHeightEqual_OutputIsosceles()
+        {
+            // Arrange
+            int sideA = 3, sideB = 5, sideC = 5;
+
+            // Act
+            var result = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
     }
 }
