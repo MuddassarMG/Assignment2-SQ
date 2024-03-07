771896d3e94988976f8a2240298dfe45d9789723
Muddassar <shaikhmuddsar786@gmail.com>
2024-03-06 19:01:31 -0500

    Add project files.

---
+++ Assignment2_SQ/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace Assignment2_SQ
+{
+    [TestFixture]
+    public class Class1
+    {
+        // Valid Equilateral Triangle
+        [Test]
+        public void Triangle_InputThreeEqualSides_OutputEquilateral()
+        {
+            // Arrange
+            int sideA = 5, sideB = 5, sideC = 5;
+
+            // Act
+            var result = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("Equilateral triangle", result);
+        }
+    }
+}
